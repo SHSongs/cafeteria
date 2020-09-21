d00b5dce7be60661d272df85d48fabbd206ce8de
Song <217.Songs.SH@gmail.com>
2020-09-21 18:12:44 +0900

    d

--- old/Pytorch/vision.py
+++ old/Pytorch/vision.py
@@ -11,18 +11,14 @@ import matplotlib.pyplot as plt
 from matplotlib.path import Path
 import matplotlib.patches as patches
 
-import cv2
-
 print('pytorch', torch.__version__)
 print('torchvision', torchvision.__version__)
 
-HumanList = []
-
 IMG_SIZE = 480
-THRESHOLD = 0.30
+THRESHOLD = 0.70
 
-img = Image.open('imgs/Cafe.jpg')
-img = img.resize((IMG_SIZE, int(img.height * IMG_SIZE / img.width))) # 압축
+img = Image.open('imgs/run.jpg')
+# img = img.resize((IMG_SIZE, int(img.height * IMG_SIZE / img.width)))
 
 plt.figure(figsize=(16, 16))
 
@@ -31,6 +27,7 @@ plt.imshow(img)
 trf = T.Compose([
     T.ToTensor()
 ])
+
 
 input_img = trf(img)
 
@@ -42,6 +39,8 @@ out = model([input_img])[0]
 
 print(out.keys())
 
+
+
 codes = [
     Path.MOVETO,
     Path.LINETO,
@@ -49,6 +48,7 @@ codes = [
 ]
 
 fig, ax = plt.subplots(1, figsize=(16, 16))
+ax.imshow(img)
 
 
 for box, score, keypoints in zip(out['boxes'], out['scores'], out['keypoints']):
@@ -62,38 +62,35 @@ for box, score, keypoints in zip(out['boxes'], out['scores'], out['keypoints']):
 
     rect = patches.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1], linewidth=2, edgecolor='b',
                              facecolor='none')
-
     ax.add_patch(rect)
 
-    # # 17 keypoints
-    # for i, k in enumerate(keypoints):
-    #     circle = patches.Circle((k[0], k[1]), radius=10, facecolor='r')
-    #     ax.add_patch(circle)
-    #     print(k)
-    #     if i > 3:
-    #         break
+    # 17 keypoints
+    for i, k in enumerate(keypoints):
+        circle = patches.Circle((k[0], k[1]), radius=10, facecolor='r')
+        ax.add_patch(circle)
+        print(k)
+        if i > 3:
+            break
 
-    # # draw path
-    # # left arm
-    # path = Path(keypoints[5:10:2], codes)
-    # line = patches.PathPatch(path, linewidth=2, facecolor='none', edgecolor='r')
-    # ax.add_patch(line)
-    #
-    # # right arm
-    # path = Path(keypoints[6:11:2], codes)
-    # line = patches.PathPatch(path, linewidth=2, facecolor='none', edgecolor='r')
-    # ax.add_patch(line)
-    #
-    # # left leg
-    # path = Path(keypoints[11:16:2], codes)
-    # line = patches.PathPatch(path, linewidth=2, facecolor='none', edgecolor='r')
-    # ax.add_patch(line)
-    #
-    # # right leg
-    # path = Path(keypoints[12:17:2], codes)
-    # line = patches.PathPatch(path, linewidth=2, facecolor='none', edgecolor='r')
-    # ax.add_patch(line)
+    # draw path
+    # left arm
+    path = Path(keypoints[5:10:2], codes)
+    line = patches.PathPatch(path, linewidth=2, facecolor='none', edgecolor='r')
+    ax.add_patch(line)
 
-ax.imshow(img.numpy())
+    # right arm
+    path = Path(keypoints[6:11:2], codes)
+    line = patches.PathPatch(path, linewidth=2, facecolor='none', edgecolor='r')
+    ax.add_patch(line)
+
+    # left leg
+    path = Path(keypoints[11:16:2], codes)
+    line = patches.PathPatch(path, linewidth=2, facecolor='none', edgecolor='r')
+    ax.add_patch(line)
+
+    # right leg
+    path = Path(keypoints[12:17:2], codes)
+    line = patches.PathPatch(path, linewidth=2, facecolor='none', edgecolor='r')
+    ax.add_patch(line)
 
 plt.show()
